8df29a8e17203641878eaee5a17847b555dc6da1
iden <idenmart07@gmail.com>
2022-11-23 18:45:43 +0600

    add post request

--- accountant/views.py
+++ accountant/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Category
+from .models import Category, Profile
 from .forms import ProfileForm
 
 
 def index_view(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        Profile.objects.create(
+            login=login,
+            password=password
+            )
     form = ProfileForm  
     return render(
         request, 
